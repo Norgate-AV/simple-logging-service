24aca35a5e0c55b0da1d64f6bb36b5a8fd055758
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-11-11 15:25:30 +0000

    feat: add basic insertion of log into db

--- models/log.go
+++ models/log.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/Norgate-AV/simple-logging-service/db"
+
 type Log struct {
 	Room    string `json:"room" binding:"required"`
 	Level   string `json:"level" binding:"required"`
@@ -7,5 +9,28 @@ type Log struct {
 }
 
 func (log *Log) Save() error {
+	query := `
+	INSERT INTO logs (room, level, message)
+	VALUES (?, ?, ?)
+	RETURNING id;
+	`
+
+	db, err := db.GetDatabase()
+	if err != nil {
+		return err
+	}
+
+	statement, err := db.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer statement.Close()
+
+	_, err = statement.Exec(log.Room, log.Level, log.Message)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
